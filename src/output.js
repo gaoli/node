090983457505622ec90ec16b3492ac189dcc2ce7
莫争 <gaoli.gl@taobao.com>
2014-02-16 21:38:40 +0800

    add comments

--- src/output.js
+++ src/output.js
@@ -4,6 +4,17 @@
  * @author 莫争 <gaoli.gl@taobao.com>
  */
 
+// ** Node 模块提供的快捷方式 **
+//
+// ```
+// mix(S, {
+//     node    : node,  // 参元类
+//     Node    : $,     // 构造器
+//     NodeList: $,     // 构造器
+//     one     : $.one, // 获取 / 创建一个 Node 对象
+//     all     : $.all  // 获取 / 创建一批 Node 对象
+// });
+// ```
 mix(S, {
     node    : node,
     Node    : $,
